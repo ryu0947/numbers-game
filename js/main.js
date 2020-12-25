ecda65d68286fea6daa5d1e91f4a6c9a0884f3f9
ryu <dragonogawaz@gmail.com>
2020-12-26 07:07:03 +0900

    li要素を動的に追加する処理

--- js/main.js
+++ js/main.js
@@ -1,1 +1,30 @@
 "use strict";
+
+{
+  class Panel {
+    constructor() {
+      this.el = document.createElement("li");
+      this.el.classList.add("pressed");
+    }
+  }
+
+  class Board {
+    constructor() {
+      this.panels = [];
+      for (let i = 1; i <= 9; i++) {
+        this.panels.push(new Panel());
+      }
+      console.log(this.panels);
+      this.setUp();
+    }
+
+    setUp() {
+      const board = document.getElementById("js-board");
+      this.panels.forEach((panel) => {
+        board.appendChild(panel.el);
+      });
+    }
+  }
+
+  const board = new Board();
+}
